45056243ff228c2de22b6f496a484b153436e513
Hamish Friedlander <hamish@silverstripe.com>
2009-10-19 21:23:52 +0000

    FEATURE: Add CompassTheme::$errors_are_errors, to allow specifing whether compass errors should cause a problem or not, and CompassTheme::$required_gems to add sit-specific compass extensions

--- code/CompassTheme.php
+++ code/CompassTheme.php
@@ -2,12 +2,28 @@
 
 class CompassTheme extends Object {
 	
+	/** Are compass errors actually errors, or should we just ignore them? True means complain, false means don't, null means don't complain on live servers, do otherwise */
+	static $errors_are_errors = null;
+	
+	/** What gems are required for compass to work? */
+	static $required_gems = array('yard', 'maruku', 'haml', 'chriseppstein-compass', 'chriseppstein-compass-colors');
+	
+	/** Internal cache variable - is the version of rubygems currently available good enough? */
 	static $gem_version_ok = null;
 	
 	static function gem_path() {
 		$path = TEMP_FOLDER . '/gems';
 		if (!file_exists($path)) mkdir($path, 0770);
 		return $path;	
+	}
+
+	static function error($message) {
+		// If errors_are_errors is null, work out if it should be true or false based on server mode
+		if (self::$errors_are_errors === null) self::$errors_are_errors = Director::isDev();
+
+		// Then raise the actual error (if errors are errors)
+		if (self::$errors_are_errors) user_error('Compass Error:<br />' . preg_replace('/[\r\n]+/', '<br />', $message), E_USER_ERROR);
+		return false;
 	}
 	
 	static function run($cmd, &$stdout, &$stderr) {
@@ -18,8 +34,7 @@ class CompassTheme extends Object {
 		);
 		 
 		$gempath = self::gem_path();
-		$flush = @$_GET['flush'] ? sprintf("FLUSH=%s ", escapeshellarg($_GET['flush'])) : '';
-		$process = proc_open("HOME='$gempath' GEM_HOME='$gempath' " . $flush . $cmd, $descriptorspec, $pipes);
+		$process = proc_open("HOME='$gempath' GEM_HOME='$gempath' " . (@$_GET['flush'] ? "FLUSH={$_GET['flush']} " : '') . $cmd, $descriptorspec, $pipes);
 		
 		$stdout = "";
 		$stderr = "";
@@ -54,10 +69,10 @@ class CompassTheme extends Object {
 		// Check that rubygems exists and is a good enough version
 		if (self::$gem_version_ok === null) {
 			$code = self::run('gem environment version', $ver, $err);
-			if ($code !== 0) user_error('Problem accessing rubygems version - is ruby installed and rubygems available?', E_USER_ERROR);
+			if ($code !== 0) return 'Ruby is present, but there was a problem accessing the current rubygems version - is rubygems available? The "gem" command needs to be in the webserver\'s path.';
 			
-			$ver = explode('.', $ver);
-			if ($ver[0] < 1 || $ver[1] < 2) user_error('Rubygems must be at least version 1.2. Please upgrade', E_USER_ERROR);
+			$vers = explode('.', $ver);
+			if ($vers[0] < 1 || $vers[1] < 2) return "Rubygems is too old. You have version $ver, but we need at least version 1.2. Please upgrade.";
 			
 			self::$gem_version_ok = true;
 		}
@@ -66,10 +81,10 @@ class CompassTheme extends Object {
 		self::run("gem list -i $gem", $out, $err);
 		if (trim($out) != 'true') {
 			$code = self::run('gem sources --add http://gems.github.com/', $out, $err);
-			if ($code !== 0) user_error("Could not add github as a gem source. Either manually add, or repair error. Error message was: $err", E_USER_ERROR);
+			if ($code !== 0) return "Could not add github as a gem source. Either manually add, or repair error. Error message was: $err";
 			
 			$code = self::run("gem install $gem", $out, $err);
-			if ($code !== 0) user_error("Could not install required gem $gem. Either manually install, or repair error. Error message was: $err", E_USER_ERROR);
+			if ($code !== 0) return "Could not install required gem $gem. Either manually install, or repair error. Error message was: $err";
 		}
 	}
 	
@@ -79,15 +94,16 @@ class CompassTheme extends Object {
 	}
 	
 	static function rebuild() {
-		// If ruby isn't present, don't throw an error, just accept it
+		// Check if ruby is present
 		$ver = `ruby --version`;
-		if (!$ver) return;
+		if (!$ver) return self::error(
+			"Ruby isn't present, and Compass can't work without it.\n".
+			"Set CompassTheme::\$errors_are_errors to false to ignore if you aren't using compass"
+		);
 
-		self::gemrequire('yard');
-		self::gemrequire('maruku');
-		self::gemrequire('haml');
-		self::gemrequire('chriseppstein-compass');
-		self::gemrequire('chriseppstein-compass-colors');
+		foreach (self::$required_gems as $gem) {
+			if ($error = self::gemrequire($gem)) return self::error($error);
+		}
 		
 		global $_CSS_MANIFEST;
 		$dirs = array();
@@ -98,7 +114,6 @@ class CompassTheme extends Object {
 			}
 		}
 
-		$gemdir = BASE_PATH.DIRECTORY_SEPARATOR.'compass'.DIRECTORY_SEPARATOR.'gems';
 		$orig = getcwd();
 		
 		foreach (array_unique($dirs) as $dir) {
@@ -112,7 +127,10 @@ class CompassTheme extends Object {
 			chdir($dir);
 			$code = self::gemrun("chriseppstein-compass", "compass", null, @$_GET['flush'] ? " --force" : "", $out, $err);
 			
-			if ($code !== 0) user_error('Compass error<br />'.preg_replace('/\n/', '<br />', $err), E_USER_ERROR);
+			if ($code !== 0) {
+				chdir($orig);
+				return self::error($err);	
+			}
 		}
 		
 		chdir($orig);
